a69df0d4ed4b95e93502b18d17fc9ec83ebdaa3d
James Harton <james@resistor.io>
2014-06-09 10:46:46 +1200

    Empty spec dir

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.formatter = :documentation
+end
